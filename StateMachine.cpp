40c074c08e9cab7289e19fdfe5ed37d55e68aaaa
bunchiochi <ochiojie@gmail.com>
2026-06-21 02:34:07 +0300

    added transition function, working on handling events

--- StateMachine.cpp
+++ StateMachine.cpp
@@ -1,22 +1,60 @@
 #include "StateMachine.h"
 #include "Arduino.h"
 
-static SystemState currentState;
-static CommsMode commsMode;
+static SystemState currentState = SystemState::ARMED;
+static CommsMode commsMode = CommsMode::CONNECTED;
+
+const char* systemStateToString(SystemState state){
+  switch(state){
+    case SystemState::IDLE:
+      return "IDLE";
+
+    case SystemState::ARMED:
+      return "ARMED";
+
+    case SystemState::TRIGGERED:
+      return "TRIGGERED";
+
+    case SystemState::CHALLENGE:
+      return "CHALLENGE";
+
+    case SystemState::LOCKED_OUT:
+      return "LOCKED_OUT";
+    default:
+      return "UNKNOWN STATE"; 
+  }
+}
 
 SystemState getCurrentState(){
   return currentState;
-};
+}
 
+CommsMode getCommsMode(){
+  return commsMode;
+}
 
+void handleEvent(Event event){
+  Serial.print(">>> [INFO] Event Handler Active...");
+
+  switch(event){
+    
+  }
+}
 
 void transitionTo(SystemState newState){
-  if (getCurrentState() == newState){
-    // checking if the new state is same as the current state
-    // should do something but not sure yet 
+  Serial.print("State Transition in progress...");
+
+  if (currentState == newState){
+    //state stays same 
+    Serial.print("[WARN] transitionTo called with same state: ");
+    Serial.println(systemStateToString(newState));
   }else{
-    if(getCurrentState() != newState){
-        currentState = newState;
+    SystemState oldState = currentState;
+    currentState = newState;
+
+    Serial.print(">>> [STATE] ");
+    Serial.print(systemStateToString(oldState));
+    Serial.print(" -> ");
+    Serial.println(systemStateToString(newState));
     }
-  }
 }
